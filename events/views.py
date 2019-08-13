c5fc8f2db10111ba6e335bf5b389eaa7dd8a24ca
habur331 <habur331@gmai.com>
2019-08-13 12:43:01 +0300

    НОРМАЛЬНЫЙПОИСК

--- events/views.py
+++ events/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.urls import reverse
+
 from .models import Event
 import datetime
 
@@ -13,16 +15,15 @@ def index(request):
 		date_end = request.POST.get('date_end')
 
 		event_list = Event.objects.all()
-
 		if city:
-			event_list.filter(event_city=city)
+			event_list = event_list.filter(event_city__in=city)
 		if theme:
-			event_list.filter(event_theme=theme)
+			event_list = event_list.filter(event_theme__in=theme)
+		if date_end:
+			event_list = event_list.filter(event_date__lte=date_end)
 		if date_start:
-			date_start = datetime.date(date_start.split('-')[0], date_start.split('-')[1], date_start.split('-')[2])
+			event_list = event_list.filter(event_date__gte=date_start)
 
-		print(event_list)
-		print(date_start)
-		return HttpResponse(event_list)
+		return render(request, 'events/event.html', context={'event_list': event_list})
 	return render(request, 'events/index.html', context={'cities': Event.cities, 'themes': Event.themes,
 														 'events': get_list_or_404(Event.objects.all())})
